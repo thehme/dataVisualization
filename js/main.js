df483f0ec4fa23b154c90ac7013b84849d6e361f
thehme <bcortez23@gmail.com>
2015-10-01 15:51:18 -0400

    revert back

--- js/main.js
+++ js/main.js
@@ -14,31 +14,22 @@ function handleFile() {
         //console.log(text);
         //console.log(text.length)
         var description = {};
-        var data = {};
+        var data = [];
         var cols = [];
         // split on new line to find all rows
         var rows = text.split('\n');
         // remove commas within quotes text
         console.log("There are: " + rows.length + " rows.");
+        
         // go through each row
-        for (var m = 0; m < rows.length; m++){
-            // traverse each row, element by element,
-            // and find those strings starting with quotes
-            for (var n  = 0; n < rows[m].length; n++) {
-                if(rows[m][n] == '"') {
-                    var startIdx = n;
-                    while (rows[m][n] != '"') {
-                        if (rows[m][n] == ',') {
-                            rows[m][n] = ' ';
-                        }
-                    } 
-                }
-            }
-        }
-        console.log("Length of row 1: " + rows[0].length);
-        console.log("Length of row 2: " + rows[1].length);
-        console.log("Length of row 25: " + rows[25].length);
-        console.log("Length of row 50: " + rows[50].length);
+        //for (var m = 0; m < rows.length; m++){
+            // go through each element of each row
+            //for (var n = 0; n < rows[m].length; n++){
+                //if (m == 0 && rows[m][n]){
+                //    var parsed = rows[m].substring(0, rows[m].indexOf('"'));
+                //}
+        //}
+        //console.log(parsed);
         // split on each comma to find all columns
         for (var i = 0; i < rows.length; i++){
             cols[i] = rows[i].split(',');

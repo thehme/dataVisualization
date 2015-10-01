3b266c0f8d4bda52f926cedd3a42394366c4d56b
thehme <bcortez23@gmail.com>
2015-10-01 13:37:20 -0400

    row length

--- js/main.js
+++ js/main.js
@@ -21,16 +21,30 @@ function handleFile() {
         // remove commas within quotes text
         console.log("There are: " + rows.length + " rows.");
         // go through each row
-        for (var m = 0; m < rows.length; rows++){
-
+        for (var m = 0; m < rows.length; m++){
+            // traverse each row, element by element,
+            // and find those strings starting with quotes
+            for (var n  = 0; n < rows[m].length; n++) {
+                if(rows[m][n] == '"') {
+                    var startIdx = n;
+                    while (rows[m][n] != '"') {
+                        if (rows[m][n] == ',') {
+                            rows[m][n] = ' ';
+                        }
+                    } 
+                }
+            }
         }
+        console.log("Length of row 1: " + rows[0].length);
+        console.log("Length of row 2: " + rows[1].length);
+        console.log("Length of row 25: " + rows[25].length);
+        console.log("Length of row 50: " + rows[50].length);
         // split on each comma to find all columns
         for (var i = 0; i < rows.length; i++){
             cols[i] = rows[i].split(',');
         }
-        console.log(cols[0].length);
-        console.log(cols[1][400]);
-        console.log(cols[1].length);
+        //console.log(cols[1][400]);
+        //console.log(cols[1].length);
     };
     // UTF-8 default encoding
     reader.readAsText(file);

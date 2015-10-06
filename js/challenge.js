587ebd24a416e5466a6c914dd8dfa5f4f2930c8a
thehme <bcortez23@gmail.com>
2015-10-05 21:33:50 -0400

    deleted old files

---
+++ js/challenge.js
@@ -0,0 +1,68 @@
+$(function(){
+
+    var dataModel = Backbone.Model.extend({
+        initialize:function() {
+
+        }
+    });
+
+    var Data = Backbone.Collection.extend({
+        model: dataModel,
+        url: function() {
+            return 'data.json';
+        },
+        initialize:function() {
+
+        },
+        parse:function(response) {
+            debugger;
+            return response.data;
+        }
+    });
+
+    var stateModel = Backbone.Model.extend({
+        initialize:function() {
+
+        }
+    });
+
+    var States = Backbone.Collection.extend({
+        model: stateModel,
+        url: function() {
+            return 'states.json';
+        },
+        initialize:function() {
+
+        },
+        parse:function(response) {
+            debugger;
+            return response.states.state;
+        }
+    });
+
+    var appView = Backbone.View.extend({
+        initialize:function() {
+            debugger;
+            this.states = new States();
+            this.states.bind('sync',this.procStates,this);
+            this.states.fetch();
+
+            this.hdata = new Data();
+            this.hdata.bind('sync',this.procData,this);
+            this.hdata.fetch(); 
+
+        },
+        procStates:function() {
+            debugger;
+        },
+        procData:function() {
+            console.log(this);
+            for(var i=0; i < this.hdata.length; i++){
+                this.hdata.models[i].set({'percentUninsured': (this.hdata.models[i].attributes.number_insured/this.hdata.models[i].attributes.population)*100});
+            }
+            debugger;
+        }
+    });
+
+    var App = new appView;
+});

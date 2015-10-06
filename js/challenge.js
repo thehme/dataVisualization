7351d0e872ccb288f9ca03499387d9ca1d6d7b9e
thehme <bcortez23@gmail.com>
2015-10-05 23:39:56 -0400

    highchart in progress

--- js/challenge.js
+++ js/challenge.js
@@ -50,6 +50,7 @@ $(function(){
             this.hdata = new Data();
             this.hdata.bind('sync',this.procData,this);
             this.hdata.fetch(); 
+            this.render();
 
         },
         procStates:function() {
@@ -61,8 +62,46 @@ $(function(){
                 this.hdata.models[i].set({'percentUninsured': (this.hdata.models[i].attributes.number_uninsured/this.hdata.models[i].attributes.population)*100});
                 this.hdata.models[i].set({'percentInsured': (this.hdata.models[i].attributes.number_insured/this.hdata.models[i].attributes.population)*100});
             }
+        },
+        render: function(){
+            $('#container').highcharts({
+                title: {
+                    text: 'Private Health Insurance Levels By State',
+                    x: -20 //center
+                },
+                xAxis: {
+                    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+                },
+                yAxis: {
+                    title: {
+                        text: 'Population (Millions)'
+                    },
+                    plotLines: [{
+                        value: 0,
+                        width: 1,
+                        color: '#808080'
+                    }]
+                },
+                legend: {
+                    layout: 'vertical',
+                    align: 'right',
+                    verticalAlign: 'middle',
+                    borderWidth: 0
+                },
+                series: [{
+                    name: 'Population',
+                    data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+                }, {
+                    name: 'Insured',
+                    data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+                }, {
+                    name: 'Uninsured',
+                    data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+                }]
+            });
             debugger;
         }
+
     });
 
     var App = new appView;
